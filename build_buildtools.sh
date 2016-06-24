c0ac10367849edba717675855a585714bed3b9c0
Ciaran Woodward <ciaran@cascoda.com>
2016-06-24 18:10:44 +0100

    Fixed typo in build script

--- build_buildtools.sh
+++ build_buildtools.sh
@@ -1,6 +1,6 @@
-cd openthread/third_party/nl_build_tools/repo/tools/packages
+cd ./openthread/third_party/nlbuild-autotools/repo/tools/packages/
 ./build
 cd ../../../../../../
-cd openthread/third_party/mbedtls/
+cd ./openthread/third_party/mbedtls/
 make
 cd ../../../
