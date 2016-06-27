651670a89be0bffe00e1f534155d5bdeb3d44548
Ciaran Woodward <ciaran@cascoda.com>
2016-06-27 10:10:20 +0100

    Fixed mbed make directory

--- build_buildtools.sh
+++ build_buildtools.sh
@@ -1,6 +1,6 @@
 cd ./openthread/third_party/nlbuild-autotools/repo/tools/packages/
 ./build
 cd ../../../../../../
-cd ./openthread/third_party/mbedtls/
+cd ./openthread/third_party/mbedtls/repo/
 make
-cd ../../../
+cd ../../../../
