aed7857ace6e33308e7745d0678142047cece70e
callum <callum@cascoda.com>
2016-06-29 14:13:48 +0100

    testing prints

--- app/main.c
+++ app/main.c
@@ -45,9 +45,15 @@ int main(int argc, char *argv[])
 
     NODE_ID = atoi(argv[1]);
 
+    char array[20] = "Hello World";
+ 
+    printf("%s\n",array);
+
     posixPlatformInit();
     otInit();
     otCliSerialInit();
+
+    printf("%s\n",array);
 
     while (1)
     {
