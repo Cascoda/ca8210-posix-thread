e79eab4d19658113bfd947225bfc1ed3acd0893f
callum <callum@cascoda.com>
2016-07-28 12:28:46 +0100

    Debugging coordChangeCallback 1.3

--- platform/radio.c
+++ platform/radio.c
@@ -281,15 +281,11 @@ void posixPlatformPostInit(void){
 }
 
 void coordChangedCallback(uint32_t aFlags, void *aContext) {
-	fprintf(stderr,"\n\r Executing coordChangeCallback \n\r");
 	if(aFlags & OT_NET_ROLE){
-		fprintf(stderr,"\n\r got past role \n\r");
 		struct SecSpec securityLevel = {0};
 		if(otGetDeviceRole() == kDeviceRoleRouter || otGetDeviceRole() == kDeviceRoleLeader){
-			fprintf(stderr,"\n\r I'm a leader or Router \n\r");
 			if(!isCoord) {
-				fprintf(stderr,"\n\r Changed Coord \n\r");
-				MLME_START_request_sync(
+				uint8_t scanRequest = MLME_START_request_sync(
 						otGetPanId(),
 						sChannel,
 						15,
@@ -300,7 +296,7 @@ void coordChangedCallback(uint32_t aFlags, void *aContext) {
 						&securityLevel,
 						&securityLevel,
 						pDeviceRef);
-
+				fprintf(stderr,"\n\r %x \n\r", scanRequest);
 				isCoord = 1;
 			}
 		} else if (isCoord) {
