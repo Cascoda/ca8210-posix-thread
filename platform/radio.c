7fda76630c7ab11db603cef05d65cd8e96957bda
callum <callum@cascoda.com>
2016-07-28 14:09:40 +0100

    Debugging scan

--- platform/radio.c
+++ platform/radio.c
@@ -139,9 +139,9 @@ ThreadError otActiveScan(uint32_t aScanChannels, uint16_t aScanDuration, otHandl
 	fprintf(stderr, "\n\r I'm executing otActiveScan \n\r");
 	//uint16_t aScanDuration = aBaseSuperframeDuration * (pow(2,ScanDuration) +1);
 	//uint8_t ScanDuration = log2((aScanDuration/aBaseSuperframeDuration) -1);
-	uint8_t ScanDuration = 8;
+	uint8_t ScanDuration = 7;
 	struct SecSpec pSecurity = {0};
-
+	fprintf(stderr, "\n %x \n", aScanChannels);
 	scanCallback = aCallback;
 	uint8_t scanRequest = MLME_SCAN_request(1, aScanChannels, ScanDuration, &pSecurity, pDeviceRef);
 	fprintf(stderr, "\n %x \n", scanRequest);
