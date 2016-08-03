ed7b26009e9a05d72c9517551e41faa6d857b721
callum <callum@cascoda.com>
2016-08-03 15:35:18 +0100

    fixing python error 2.1

--- python_server.py
+++ python_server.py
@@ -3,9 +3,9 @@ import socket
 UDP_IP = "::" # = 0.0.0.0 u IPv4
 UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
+sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, 0)
 sock.bind((UDP_IP, UDP_PORT, 0, 0))
 
 while True:
 	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-	print "received message:", data
+	print ("received message: " + data)
