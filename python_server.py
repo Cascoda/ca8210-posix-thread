9fcdbfe58540ef5f10534fa886880a6cb28a778c
callum <callum@cascoda.com>
2016-08-03 14:01:49 +0100

    fixing python error

--- python_server.py
+++ python_server.py
@@ -3,8 +3,7 @@ import socket
 UDP_IP = "::" # = 0.0.0.0 u IPv4
 UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET6, # Internet
-						socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT, 0, 0))
 
 while True:
