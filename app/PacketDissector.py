29e0fed32e296f945663d7f8d47cf5e971c6a3b9
Ciaran Woodward <ciaran@cascoda.com>
2016-09-21 17:59:33 +0100

    added comments to the packet dissector

--- app/PacketDissector.py
+++ app/PacketDissector.py
@@ -9,60 +9,75 @@ def printDissected( packetString ):
 	global doc
 	inData = bytearray.fromhex(packetString)
 	iterCounter = 0
+	#Look though every item to find the description for the correct command ID
 	for item in doc['List']['RadioPacket']:
-			if int(item['byte']['#text'], 0) == inData[0]:
-					print item['@Name']
-					iterCounter = 2
-					for param in item['List']['Parameter']:
-							s = param['@Name']
-							displayLengthCount = 0
-							displayLengthMax = 255
+		if int(item['byte']['#text'], 0) == inData[0]:
+			#Found command, print the name
+			print item['@Name']
+			#Skip command and length bytes
+			iterCounter = 2
+			#Go through all of the parameters and print them
+			for param in item['List']['Parameter']:
+				s = param['@Name']
 
-							visible = True
+				#Displaylength controls how many characters are printed, but many fields still need to be fully read
+				displayLengthCount = 0
+				displayLengthMax = 255
 
-							if 'DisplayLength' in param:
-								displayLengthMax = int(param['DisplayLength'], 0)
+				#Visibility hides parameters which are included in the packet but not used
+				visible = True
 
-							if 'Visibility' in param:
-								if str(param['Visibility']) == 'false':
-									visible = False
+				#Apply displaylength if relevent
+				if 'DisplayLength' in param:
+					displayLengthMax = int(param['DisplayLength'], 0)
 
-							for _ in itertools.repeat(None, int(param['@Length'], 0)):
-								if displayLengthCount < displayLengthMax:
-									s += ' ' + hex(inData[iterCounter]) + ' '
-									displayLengthCount += 1
+				#Apply Visibility if relevent
+				if 'Visibility' in param:
+					if str(param['Visibility']) == 'false':
+						visible = False
 
-								if param['@Type'] == 'Discrete':
-									for ValueDes in param['PotentialValues']['Value']:
-										if int(ValueDes['@Value'], 0) == inData[iterCounter]:
-											s += '(' + ValueDes['@Name'] + ')'
+				#This loop handles scrolling through the received bytes and printing them
+				for _ in itertools.repeat(None, int(param['@Length'], 0)):
+					if displayLengthCount < displayLengthMax:
+						s += ' ' + hex(inData[iterCounter]) + ' '
+						displayLengthCount += 1
 
-											if 'ParameterDependancy' in ValueDes:
-												for parDep in ValueDes['ParameterDependancy']:
-													for targetPar in item['List']['Parameter']:
-														if targetPar['@Name'] == str(parDep['@Name']):
-															if '#text' in parDep:
-																targetPar[str(parDep['@Value'])] = parDep['#text']
-															else:
-																targetPar['@' + str(parDep['@Value'])] = hex(inData[iterCounter])
-											break;
+					#Certain parameters are discrete, and come with accompanying descriptions
+					if param['@Type'] == 'Discrete':
+						for ValueDes in param['PotentialValues']['Value']:
+							if int(ValueDes['@Value'], 0) == inData[iterCounter]:
+								s += '(' + ValueDes['@Name'] + ')'
 
-								iterCounter += 1
-								if iterCounter >= len(inData):
-									if visible:
-										print s
-									return;
-							if visible:
-								print s
-							if 'ParameterDependancy' in param:
-								for parDep in param['ParameterDependancy']:
-									for targetPar in item['List']['Parameter']:
-										if targetPar['@Name'] == str(parDep['@Name']):
-											if '#text' in parDep:
-												targetPar[str(parDep['@Value'])] = parDep['#text']
-											else:
-												targetPar['@' + str(parDep['@Value'])] = hex(inData[iterCounter-1])
+								#Discrete parameters can have effects on the properties of upcoming parameters
+								if 'ParameterDependancy' in ValueDes:
+									for parDep in ValueDes['ParameterDependancy']:
+										for targetPar in item['List']['Parameter']:
+											if targetPar['@Name'] == str(parDep['@Name']):
+												if '#text' in parDep:
+													targetPar[str(parDep['@Value'])] = parDep['#text']
+												else:
+													targetPar['@' + str(parDep['@Value'])] = hex(inData[iterCounter])
+								break;
 
+					iterCounter += 1
+					if iterCounter >= len(inData):
+						if visible:
+							print s
+						return;
+				if visible:
+					print s
+
+				#Some parameter dependancies can cause a future property to be affected by the value of itself (eg length)
+				if 'ParameterDependancy' in param:
+					for parDep in param['ParameterDependancy']:
+						for targetPar in item['List']['Parameter']:
+							if targetPar['@Name'] == str(parDep['@Name']):
+								if '#text' in parDep:
+									targetPar[str(parDep['@Value'])] = parDep['#text']
+								else:
+									targetPar['@' + str(parDep['@Value'])] = hex(inData[iterCounter-1])
+
+	#Print leftover data (this should never happen)
 	if iterCounter < len(inData):
 		s = ' '
 		for _ in itertools.repeat(None, len(inData) - iterCounter):
@@ -74,9 +89,12 @@ def printDissected( packetString ):
 
 def main():
 	global doc
+
+	#Open and parse the XML file
 	with open(XML) as fd:
 		doc = xmltodict.parse(fd.read(), force_list=('ParameterDependancy', 'Parameter', 'Value',))
 
+	#Open the logfile and parse it line by line
 	with open(LOG) as fp:
 		for line in fp:
 			line = line.rstrip()
